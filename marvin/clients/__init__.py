dc05b9adbf3db465e2c5aff4dccf579cd92e02af
Jared Mashburn <jmashburn@iwobble.com>
2025-05-04 23:44:48 -0400

    Adding Marvin Clients

---
+++ marvin/clients/__init__.py
@@ -0,0 +1,10 @@
+from marvin.core.config import get_app_dirs, get_app_plugins, get_app_settings
+from marvin.core.root_logger import get_logger
+
+
+class BaseClient:
+    def __init__(self) -> None:
+        self._logger = get_logger()
+        self._settings = get_app_settings()
+        self._directories = get_app_dirs()
+        self._plugins = get_app_plugins()
