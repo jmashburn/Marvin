39e6543d63ba3f97a7b96a3e1438b27b10043dcf
Jared Mashburn <jmashburn@iwobble.com>
2025-05-19 22:50:51 -0400

    Update validator_controller.pyp

--- marvin/routes/validators/validator_controller.py
+++ marvin/routes/validators/validator_controller.py
@@ -4,9 +4,9 @@ from fastapi import APIRouter, Depends
 from slugify import slugify
 from sqlalchemy.orm.session import Session
 
-from mealie.db.db_setup import generate_session
-from mealie.repos.all_repositories import get_repositories
-from mealie.schema.response import ValidationResponse
+from marvin.db.db_setup import generate_session
+from marvin.repos.all_repositories import get_repositories
+from marvin.schema.response import ValidationResponse
 
 router = APIRouter()
 
@@ -14,7 +14,7 @@ router = APIRouter()
 @router.get("/user/name", response_model=ValidationResponse)
 def validate_user(name: str, session: Session = Depends(generate_session)):
     """Checks if a user with the given name exists"""
-    db = get_repositories(session, group_id=None, household_id=None)
+    db = get_repositories(session, group_id=None)
     existing_element = db.users.get_one(name, "username", any_case=True)
     return ValidationResponse(valid=existing_element is None)
 
@@ -22,7 +22,7 @@ def validate_user(name: str, session: Session = Depends(generate_session)):
 @router.get("/user/email", response_model=ValidationResponse)
 def validate_user_email(email: str, session: Session = Depends(generate_session)):
     """Checks if a user with the given name exists"""
-    db = get_repositories(session, group_id=None, household_id=None)
+    db = get_repositories(session, group_id=None)
     existing_element = db.users.get_one(email, "email", any_case=True)
     return ValidationResponse(valid=existing_element is None)
 
@@ -30,7 +30,7 @@ def validate_user_email(email: str, session: Session = Depends(generate_session)
 @router.get("/group", response_model=ValidationResponse)
 def validate_group(name: str, session: Session = Depends(generate_session)):
     """Checks if a group with the given name exists"""
-    db = get_repositories(session, group_id=None, household_id=None)
+    db = get_repositories(session, group_id=None)
     existing_element = db.groups.get_by_name(name)
     return ValidationResponse(valid=existing_element is 
 None)
